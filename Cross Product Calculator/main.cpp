5bf2ec449808180f1c50cf170807e95363008f7b
tylerdoll <tdoll1997@gmail.com>
2015-09-28 22:46:54 -0600

    Initial Commit

---
+++ Cross Product Calculator/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Cross Product Calculator
+//
+//  Created by Tyler Doll on 9/28/15.
+//  Copyright © 2015 Tyler Doll. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
